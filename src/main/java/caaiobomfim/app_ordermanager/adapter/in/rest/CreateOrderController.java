4399b95b5376b98ee582a3c62340c8976392c43e
Caio Bomfim <caaiobomfim@gmail.com>
2025-07-02 22:14:59 -0300

    feat(domain): :rocket: gerar id randomico e instanciar order

--- src/main/java/caaiobomfim/app_ordermanager/adapter/in/rest/CreateOrderController.java
+++ src/main/java/caaiobomfim/app_ordermanager/adapter/in/rest/CreateOrderController.java
@@ -2,6 +2,7 @@ package caaiobomfim.app_ordermanager.adapter.in.rest;
 
 import caaiobomfim.app_ordermanager.adapter.in.rest.dto.OrderRequest;
 import caaiobomfim.app_ordermanager.adapter.in.rest.dto.OrderResponse;
+import caaiobomfim.app_ordermanager.domain.model.Order;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,8 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/pedidos")
@@ -18,7 +18,10 @@ public class CreateOrderController {
 
     @PostMapping
     public ResponseEntity<OrderResponse> createOrder(@Valid @RequestBody OrderRequest request) {
-        List<String> items = new ArrayList<>();
+
+        String id = UUID.randomUUID().toString();
+        Order order = new Order(id, request.getClientId(), request.getItems(), "PENDENTE");
+
         OrderResponse response = new OrderResponse();
         return ResponseEntity.status(201).body(response);
     }
